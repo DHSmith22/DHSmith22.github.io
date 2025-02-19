27df13d376617b71fa56f9d891b3224eda5e5ebc
Dorian Smith <Smith,h.dorian@gmail.com>
2025-02-19 12:16:54 -0600

    turned on JS intro, oops

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,70 +1,70 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//     const introContainer = document.querySelector('.intro-container');
-//     const introWrapperText = document.getElementById('intro-text');
-//     const heroWrapper = document.querySelector('.hero-wrapper');
-//     const projectsPage = document.querySelector('.projects-page');
-//     const contactPage = document.querySelector('.contact-page');
+document.addEventListener("DOMContentLoaded", function(){
+    const introContainer = document.querySelector('.intro-container');
+    const introWrapperText = document.getElementById('intro-text');
+    const heroWrapper = document.querySelector('.hero-wrapper');
+    const projectsPage = document.querySelector('.projects-page');
+    const contactPage = document.querySelector('.contact-page');
 
-//     setTimeout(() => {
-//         projectsPage.classList.add('hidden');
-//         contactPage.classList.add('hidden');
-//         heroWrapper.classList.add('hidden');
-//         introWrapperText.style.transition = '.35s';
-//         introWrapperText.innerHTML="Hej";
-//     }, 500);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="مرحبًا";
-//     }, 700);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Olá";
-//     }, 900);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="你好";
-//     }, 1100);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="こんにちは";
-//     }, 1300);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Bonjour";
-//     }, 1500);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Привет";
-//     }, 1700);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="नमस्ते";
-//     }, 1900);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="H";
-//     }, 2100);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="He";
-//     }, 2200);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Hel";
-//     }, 2300);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Hell";
-//     }, 2400);
-//     setTimeout(() => {
-//         introWrapperText.innerHTML="Hello";
-//     }, 2500);
-//     setTimeout(() => {
-//         introWrapperText.style.scale = '1.5';
-//     }, 2600);
-//     setTimeout(() => {
-//         introWrapperText.style.scale = '1'
-//         introWrapperText.style.opacity = '0'
-//         heroWrapper.classList.remove('hidden');
-//         heroWrapper.scrollIntoView({behavior: 'smooth'});
-//     }, 3500);
-//     setTimeout(() => {
-//         introContainer.classList.add('hidden');
-//     }, 4000);
-//     setTimeout(() => {
-//        projectsPage.classList.remove('hidden');
-//        contactPage.classList.remove('hidden');
-//     }, 4250);
-// });
+    setTimeout(() => {
+        projectsPage.classList.add('hidden');
+        contactPage.classList.add('hidden');
+        heroWrapper.classList.add('hidden');
+        introWrapperText.style.transition = '.35s';
+        introWrapperText.innerHTML="Hej";
+    }, 500);
+    setTimeout(() => {
+        introWrapperText.innerHTML="مرحبًا";
+    }, 700);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Olá";
+    }, 900);
+    setTimeout(() => {
+        introWrapperText.innerHTML="你好";
+    }, 1100);
+    setTimeout(() => {
+        introWrapperText.innerHTML="こんにちは";
+    }, 1300);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Bonjour";
+    }, 1500);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Привет";
+    }, 1700);
+    setTimeout(() => {
+        introWrapperText.innerHTML="नमस्ते";
+    }, 1900);
+    setTimeout(() => {
+        introWrapperText.innerHTML="H";
+    }, 2100);
+    setTimeout(() => {
+        introWrapperText.innerHTML="He";
+    }, 2200);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Hel";
+    }, 2300);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Hell";
+    }, 2400);
+    setTimeout(() => {
+        introWrapperText.innerHTML="Hello";
+    }, 2500);
+    setTimeout(() => {
+        introWrapperText.style.scale = '1.5';
+    }, 2600);
+    setTimeout(() => {
+        introWrapperText.style.scale = '1'
+        introWrapperText.style.opacity = '0'
+        heroWrapper.classList.remove('hidden');
+        heroWrapper.scrollIntoView({behavior: 'smooth'});
+    }, 3500);
+    setTimeout(() => {
+        introContainer.classList.add('hidden');
+    }, 4000);
+    setTimeout(() => {
+       projectsPage.classList.remove('hidden');
+       contactPage.classList.remove('hidden');
+    }, 4250);
+});
 
 // Dark/light mode Toggle
 
